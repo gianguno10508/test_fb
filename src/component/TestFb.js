837f1dc4b705014ac3339b9b3a6935599fd5251e
GiangNV <giangnv@arrowhitech.co>
2022-10-13 17:11:42 +0700

    dfgf

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -1,25 +1,40 @@
-import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook, ShareButton, Comments, Like, LoginButton } from 'react-facebook';
-import MessengerCustomerChat from 'react-messenger-customer-chat';
-export default function EmbeddedPostExample() {
-  function handleSuccess(response) {
-    console.log(response.status);
-    console.log('done');
+// import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook, ShareButton, Comments, Like, LoginButton } from 'react-facebook';
+// import MessengerCustomerChat from 'react-messenger-customer-chat';
+// export default function EmbeddedPostExample() {
+//   function handleSuccess(response) {
+//     console.log(response.status);
+//     console.log('done');
+//   }
+
+//   function handleError(error) {
+//     console.log(error);
+//     console.log('error');
+//   }
+//   return (
+//     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
+//     <FacebookProvider appId="1505620756529166">
+//       <LoginButton
+//         scope="email"
+//         onError={handleError}
+//         onSuccess={handleSuccess}
+//       >
+//         Login via Facebook
+//       </LoginButton>
+//     </FacebookProvider>
+//   );
+// }
+import { FacebookProvider, useShare } from 'react-facebook';
+
+export default function ShareExample() {
+  const { share, isLoading, error } = useShare();
+
+  async function handleShare() {
+    await share({
+      href: 'http://www.facebook.com',
+    });
   }
 
-  function handleError(error) {
-    console.log(error);
-    console.log('error');
-  }
   return (
-    //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
-    <FacebookProvider appId="1505620756529166">
-      <LoginButton
-        scope="email"
-        onError={handleError}
-        onSuccess={handleSuccess}
-      >
-        Login via Facebook
-      </LoginButton>
-    </FacebookProvider>
+    <button type="button" disabled={isLoading} onClick={handleShare}>Share</button>
   );
 }
