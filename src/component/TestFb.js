083bd67c44793d3731224fc0c71386700cee9602
GiangNV <giangnv@arrowhitech.co>
2022-10-13 16:39:26 +0700

    dffgdf

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -4,7 +4,7 @@ export default function EmbeddedPostExample() {
   return (
     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
     <FacebookProvider appId="1505620756529166">
-        <ShareButton href="http://www.facebook.com" className="my-classname">
+        <ShareButton href="https://www.facebook.com/Pingg-Shop-107400511790455" className="my-classname">
           Share
         </ShareButton>
     </FacebookProvider>
