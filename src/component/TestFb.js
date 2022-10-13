48b592013f871127d85bc95e625e5940dba94d9e
GiangNV <giangnv@arrowhitech.co>
2022-10-13 17:32:23 +0700

    dffgf

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -3,6 +3,12 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 export default function EmbeddedPostExample() {
   return (
     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
-    <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=450&layout=standard&action=like&size=small&share=true&height=35&appId=1505620756529166" width="450" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
+    <div 
+      className="fb-like" 
+      data-href="https://developers.facebook.com/docs/plugins/" 
+      data-width="" 
+      data-layout="standard" 
+      data-action="like" data-size="small" data-share="true">
+    </div>
   );
 }
