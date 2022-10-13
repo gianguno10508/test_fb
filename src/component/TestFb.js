ee43b6e999a71694e42c2e1c34ba153b6ebf1184
GiangNV <giangnv@arrowhitech.co>
2022-10-13 16:47:04 +0700

    dffgf

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -4,9 +4,7 @@ export default function EmbeddedPostExample() {
   return (
     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
     <FacebookProvider appId="1505620756529166">
-        <ShareButton href="https://www.facebook.com/Pingg-Shop-107400511790455" className="my-classname">
-          Share
-        </ShareButton>
+        <Comments href="https://www.facebook.com/permalink.php?story_fbid=pfbid02dCvSYZaFsi38kSJ5x3fybX8QjuhSNwSNc4vpdSNKeyNbPCZdRN6wTCpNYAmumGsHl&id=100026930804116" />
     </FacebookProvider>
   );
 }
