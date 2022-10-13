be92d752cf8fa4d3a42e696dc5ee75a45cce256d
GiangNV <giangnv@arrowhitech.co>
2022-10-13 16:51:06 +0700

    dgfg

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -1,9 +1,10 @@
-import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook, ShareButton, Comments } from 'react-facebook';
+import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook, ShareButton, Comments, Like } from 'react-facebook';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
 export default function EmbeddedPostExample() {
   return (
     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
     <FacebookProvider appId="1505620756529166">
+        <Like href="https://www.facebook.com/permalink.php?story_fbid=pfbid02dCvSYZaFsi38kSJ5x3fybX8QjuhSNwSNc4vpdSNKeyNbPCZdRN6wTCpNYAmumGsHl&id=100026930804116" colorScheme="dark" showFaces share />
         <Comments href="https://www.facebook.com/permalink.php?story_fbid=pfbid02dCvSYZaFsi38kSJ5x3fybX8QjuhSNwSNc4vpdSNKeyNbPCZdRN6wTCpNYAmumGsHl&id=100026930804116" />
     </FacebookProvider>
   );
