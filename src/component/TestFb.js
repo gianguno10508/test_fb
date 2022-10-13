c13ad1c513bb4a089447c25f49e3ece7e8cefda1
GiangNV <giangnv@arrowhitech.co>
2022-10-13 17:21:51 +0700

    dfgf

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -1,40 +1,8 @@
-// import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook, ShareButton, Comments, Like, LoginButton } from 'react-facebook';
-// import MessengerCustomerChat from 'react-messenger-customer-chat';
-// export default function EmbeddedPostExample() {
-//   function handleSuccess(response) {
-//     console.log(response.status);
-//     console.log('done');
-//   }
-
-//   function handleError(error) {
-//     console.log(error);
-//     console.log('error');
-//   }
-//   return (
-//     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
-//     <FacebookProvider appId="1505620756529166">
-//       <LoginButton
-//         scope="email"
-//         onError={handleError}
-//         onSuccess={handleSuccess}
-//       >
-//         Login via Facebook
-//       </LoginButton>
-//     </FacebookProvider>
-//   );
-// }
-import { FacebookProvider, useShare } from 'react-facebook';
-
-export default function ShareExample() {
-  const { share, isLoading, error } = useShare();
-
-  async function handleShare() {
-    await share({
-      href: 'http://www.facebook.com',
-    });
-  }
-
+import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook, ShareButton, Comments, Like, LoginButton } from 'react-facebook';
+import MessengerCustomerChat from 'react-messenger-customer-chat';
+export default function EmbeddedPostExample() {
   return (
-    <button type="button" disabled={isLoading} onClick={handleShare}>Share</button>
+    //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
+    <iframe src="https://www.facebook.com/plugins/like.php?href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2Fplugins%2F&width=450&layout=standard&action=like&size=small&share=true&height=35&appId=1505620756529166" width="450" height="35" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share"></iframe>
   );
 }
