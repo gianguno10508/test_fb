8eceeac35130fe119554c03424db9fa6a6fb3c80
GiangNV <giangnv@arrowhitech.co>
2022-10-13 17:00:16 +0700

    dffg

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -3,10 +3,12 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 export default function EmbeddedPostExample() {
   function handleSuccess(response) {
     console.log(response.status);
+    console.log('done');
   }
 
   function handleError(error) {
     console.log(error);
+    console.log('error');
   }
   return (
     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
