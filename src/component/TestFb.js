e01f851dcd95b009cc3b8a7bd77fd076e2aa6ee4
GiangNV <giangnv@arrowhitech.co>
2022-10-14 17:04:08 +0700

    dferqretrty

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -3,20 +3,15 @@ import MessengerCustomerChat from 'react-messenger-customer-chat';
 export default function EmbeddedPostExample() {
   return (
     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
-    // <div
-    //   className="fb-like" 
-    //   data-href="https://www.24h.com.vn/tin-tuc-trong-ngay/chinh-thuc-bao-so-5-hinh-thanh-tren-bien-dong-mien-trung-mua-nhu-trut-nuoc-c46a1405400.html" 
-    //   data-width="100" 
-    //   data-layout="standard" 
-    //   data-action="like" data-size="small" data-share="true">
-    // </div>
-          <div 
-          class="fb-share-button" 
+    <>
+        <div
+          className="fb-like" 
           data-href="https://www.24h.com.vn/tin-tuc-trong-ngay/chinh-thuc-bao-so-5-hinh-thanh-tren-bien-dong-mien-trung-mua-nhu-trut-nuoc-c46a1405400.html" 
-          data-layout="button_count"
-          data-action="like" data-size="small">
-          <a target="_blank" href="https://www.24h.com.vn/tin-tuc-trong-ngay/chinh-thuc-bao-so-5-hinh-thanh-tren-bien-dong-mien-trung-mua-nhu-trut-nuoc-c46a1405400.html" class="fb-xfbml-parse-ignore">
-          Chia sẻ</a>
-          </div>
+          data-width="100" 
+          data-layout="standard" 
+          data-action="like" data-size="small" data-share="true">
+      </div>
+      <div class="fb-comments" data-href="https://www.24h.com.vn/tin-tuc-trong-ngay/chinh-thuc-bao-so-5-hinh-thanh-tren-bien-dong-mien-trung-mua-nhu-trut-nuoc-c46a1405400.html/" data-width="" data-numposts="5"></div>
+    </>
   );
 }
