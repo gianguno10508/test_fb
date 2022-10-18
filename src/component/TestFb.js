fd947d4b8b6edcce78b85edbf92bbf0ae03241cd
GiangNV <giangnv@arrowhitech.co>
2022-10-18 09:35:41 +0700

    dter

--- src/component/TestFb.js
+++ src/component/TestFb.js
@@ -1,5 +1,9 @@
 import { FacebookProvider, EmbeddedPost, Page,Feed, useFacebook, ShareButton, Comments, Like, LoginButton } from 'react-facebook';
 import MessengerCustomerChat from 'react-messenger-customer-chat';
+import FacebookLogin from 'react-facebook-login';
+const responseFacebook = (response) => {
+  console.log(response);
+}
 export default function EmbeddedPostExample() {
   return (
     //<MessengerCustomerChat pageId="107400511790455" appId="1505620756529166"/>
@@ -13,6 +17,15 @@ export default function EmbeddedPostExample() {
       </div>
       <div className="fb-comments" data-href="https://www.24h.com.vn/tin-tuc-trong-ngay/chinh-thuc-bao-so-5-hinh-thanh-tren-bien-dong-mien-trung-mua-nhu-trut-nuoc-c46a1405400.html/" data-width="" data-numposts="5"></div>
       <div className="fb-page" data-href="https://www.facebook.com/Pingg-Shop-107400511790455" data-tabs="" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true"><blockquote cite="https://www.facebook.com/Pingg-Shop-107400511790455" className="fb-xfbml-parse-ignore"><a href="https://www.facebook.com/Pingg-Shop-107400511790455">Pingg Shop</a></blockquote></div>
+
+      <FacebookLogin
+        appId="1505620756529166"
+        autoLoad={true}
+        fields="name,email,picture"
+        callback={responseFacebook}
+        cssClass="my-facebook-button-class"
+        icon="fa-facebook"
+      />
     </>
   );
 }
